69d5d52972eef2c04fcfcaae7ee49f3aef7bc512
KIRK WALLS <KIRWALLS@uat.edu>
2019-11-04 12:28:06 -0700

    Initial Commit

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private Transform tf;
+
+    public float speed = 1.0f;
+
+    private float xMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xMovement = Input.GetAxis("Horizontal") * speed;
+
+        tf.position = new Vector2(xMovement, 0f).normalized;
+    }
+}
